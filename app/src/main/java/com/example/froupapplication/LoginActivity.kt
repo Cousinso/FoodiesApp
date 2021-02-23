9c42c07c0f9ffa74df4600340834372f0e8929da
Saul Yu <saulxaviercyu@gmail.com>
2021-02-23 16:33:54 +0000

    first commit

---
+++ app/src/main/java/com/example/froupapplication/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.froupapplication
+
+import android.os.Bundle
+import android.view.AbsSavedState
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/example/froupapplication/MainActivity.kt
+++ app/src/main/java/com/example/froupapplication/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.froupapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Instantiating UI elements
+        val email = findViewById<EditText>(R.id.emailEditTextRegister)
+        val password = findViewById<EditText>(R.id.passwordEditTextRegister)
+        val register = findViewById<Button>(R.id.registerButtonRegister)
+        val login = findViewById<TextView>(R.id.loginTextViewRegister)
+
+        register.setOnClickListener {
+            Log.d("MainActivity", "Email: ${email.text.toString()}")
+            Log.d("MainActivity", "Password: ${password.text.toString()}")
+        }
+
+        login.setOnClickListener {
+            Log.d("MainActivity", "Login pressed!")
+
+            // Go to login activity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
