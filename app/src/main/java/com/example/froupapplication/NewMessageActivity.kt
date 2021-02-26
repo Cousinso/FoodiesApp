4103230de6274eb8ddd88f4c525b3bb5e7cac4ec
Saul Yu <saulxaviercyu@gmail.com>
2021-02-26 12:14:40 +0000

    profile pictures now showing

--- app/src/main/java/com/example/froupapplication/NewMessageActivity.kt
+++ app/src/main/java/com/example/froupapplication/NewMessageActivity.kt
@@ -1,18 +1,18 @@
 package com.example.froupapplication
 
-import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupieAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.user_row_new_message.view.*
 
 class NewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,9 +48,10 @@ class NewMessageActivity : AppCompatActivity() {
     }
 }
 
-class UserItem(val user: User): Item<GroupieViewHolder>() {
+class UserItem(val user: User) : Item<GroupieViewHolder>() {
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-
+        viewHolder.itemView.usernameTextViewNewMessage.text = user.username
+        Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.photoImageViewNewMessages)
     }
     override fun getLayout(): Int {
         return R.layout.user_row_new_message
