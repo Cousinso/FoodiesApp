92ac77652d79912b56a17b3f5eb95d2c8889ba66
Saul Yu <saulxaviercyu@gmail.com>
2021-04-08 12:00:46 +0100

    based off James' branch; added error handling on other activities;

--- app/src/main/java/com/example/froupapplication/LatestMessageRow.kt
+++ app/src/main/java/com/example/froupapplication/LatestMessageRow.kt
@@ -28,7 +28,9 @@ class LatestMessageRow(val chatMessage: ChatMessage): Item<GroupieViewHolder>(){
                 otherPersonUser = snapshot.getValue(User::class.java)
                 viewHolder.itemView.username_textview_latest_message.text = otherPersonUser?.username
                 val targetImage = viewHolder.itemView.imageView_latest_message
-                Picasso.get().load(otherPersonUser?.profileImageUrl).into(targetImage)
+                if (otherPersonUser?.profileImageUrl != "") {
+                    Picasso.get().load(otherPersonUser?.profileImageUrl).into(targetImage)
+                }
             }
 
             override fun onCancelled(error: DatabaseError) {
