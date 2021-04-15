e23270fecff8a782a485a50e4be6f2e566a809f6
Saul Yu <saulxaviercyu@gmail.com>
2021-04-15 17:55:05 +0100

    count of number of users in food select not showing;

--- app/src/main/java/com/example/froupapplication/WaitingForGroupActivity.kt
+++ app/src/main/java/com/example/froupapplication/WaitingForGroupActivity.kt
@@ -2,22 +2,46 @@ package com.example.froupapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import com.google.firebase.database.*
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_selected_food.*
 import kotlinx.android.synthetic.main.activity_waiting_for_group.*
 
 class WaitingForGroupActivity : AppCompatActivity() {
+    companion object {
+        val currentFood: Food? = null
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_waiting_for_group)
 
-        val food = intent.getParcelableExtra<Food>(SelectedFoodActivity.FOOD_KEY)
+        val currentFood = intent.getParcelableExtra<Food>(SelectedFoodActivity.FOOD_KEY)
 
-        supportActionBar?.title = food?.name
+        supportActionBar?.title = currentFood?.name
 
-        if (food?.foodImageUrl != "") {
-            Picasso.get().load(food?.foodImageUrl).into(foodImageImageViewWaitingForGroupActivity)
+        if (currentFood?.foodImageUrl != "") {
+            Picasso.get().load(currentFood?.foodImageUrl).into(foodImageImageViewWaitingForGroupActivity)
         }
+
+        waitForGroup()
+    }
+
+    private fun waitForGroup () {
+        val fid = currentFood?.fid
+        val ref = FirebaseDatabase.getInstance().getReference("/foods/$fid")
+
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.d("WaitingForGroupActivity", "Number of users in food group: ${snapshot.childrenCount}")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 }
