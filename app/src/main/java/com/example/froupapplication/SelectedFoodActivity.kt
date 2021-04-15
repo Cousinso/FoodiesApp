48c61874e7372f3567039f97de450df704e794cc
Saul Yu <saulxaviercyu@gmail.com>
2021-04-15 15:30:15 +0100

    selecting and confirming food now pushes users to foods;

--- app/src/main/java/com/example/froupapplication/SelectedFoodActivity.kt
+++ app/src/main/java/com/example/froupapplication/SelectedFoodActivity.kt
@@ -3,6 +3,12 @@ package com.example.froupapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_selected_food.*
 
@@ -24,11 +30,28 @@ class SelectedFoodActivity : AppCompatActivity() {
         }
         descriptionTextViewSelectedFood.text = food?.name
 
+        val currentUser = LatestMessagesActivity.currentUser
+
+        val uid = currentUser?.uid
+        val username = currentUser?.username
+
+        Log.d("SelectedFoodActivity", "Current UID: $uid")
+        Log.d("SelectedFoodActivity", "Current User: $username")
+
         confirmButtonSelectedFood.setOnClickListener {
+            val ref = FirebaseDatabase.getInstance().getReference("/foods/${food?.fid}/users").push()
+
+            val user = UserGroupItem(uid!!, username!!)
+            ref.setValue(user)
+
             val intent = Intent(it.context, WaitingForGroupActivity::class.java)
             intent.putExtra(FOOD_KEY, food)
             startActivity(intent)
             finish()
         }
     }
+}
+
+private class UserGroupItem(val uid: String, val name: String){
+    constructor() : this("", "")
 }
