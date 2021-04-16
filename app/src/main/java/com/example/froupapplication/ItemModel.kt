8ab68eeee9211c21dfdad657d3f6b4802613fa18
Adam Keating <keatina4@tcd.ie>
2021-04-16 15:21:50 +0100

    Adam - UI

---
+++ app/src/main/java/com/example/froupapplication/ItemModel.kt
@@ -0,0 +1,22 @@
+package com.example.froupapplication
+
+import com.squareup.picasso.RequestCreator
+
+class ItemModel {
+    var image = 0
+        private set
+    var nama: String? = null
+        private set
+    var usia: String? = null
+        private set
+    var kota: String? = null
+        private set
+
+    constructor() {}
+    constructor(image: Int, nama: String, usia: String?, kota: String?) {
+        this.image = image
+        this.nama = nama
+        this.usia = usia
+        this.kota = kota
+    }
+}
