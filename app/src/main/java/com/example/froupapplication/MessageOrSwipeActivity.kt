ba9ee33959d8c26d377773b2fc976749fd0cbe15
GH-James <goodingj@tcd.ie>
2021-04-17 10:49:56 +0100

    kinda get user

--- app/src/main/java/com/example/froupapplication/MessageOrSwipeActivity.kt
+++ app/src/main/java/com/example/froupapplication/MessageOrSwipeActivity.kt
@@ -3,8 +3,10 @@ package com.example.froupapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import com.google.firebase.database.FirebaseDatabase
 
 class MessageOrSwipeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,12 +18,22 @@ class MessageOrSwipeActivity : AppCompatActivity() {
         val text = findViewById<TextView>(R.id.UserTextView)
         text.setText("uid")
         message.setOnClickListener{
-            val intent = Intent(this, ChatLogActivity::class.java)
+            var toUser: User? = null
+            FirebaseDatabase.getInstance().getReference("/users/$uid").get().addOnSuccessListener {
+                Log.d("GroupChatLogActivity","Got ${it.value}")
+                var uri = ""
+                if(it.value != null){
+                    val map = it.value as HashMap<*,*>
+                    Log.d("GroupChatLogActivity","Clicker profile, got map $map")
+                    val toUser = User(map.get("uid").toString(),map.get("username").toString(),map.get("profileImageUrl").toString())
 
+                    val myContext = context
+                    val intent = Intent(myContext, ProfileTestActivity::class.java)
+                    intent.putExtra("User", toUser)
+                    myContext.startActivity(intent)
+                }
 
-            intent.putExtra(NewMessageActivity.USER_KEY, otherUser)
-            startActivity(intent)
-            startActivity(intent)
+            }
         }
         swipe.setOnClickListener{
             finish()
